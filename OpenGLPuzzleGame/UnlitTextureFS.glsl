1ab63b193253c6212ce238b4c0372bac3b1f2c2b
Dandarawy <abdulla.aldandarawy@gmail.com>
2016-10-15 00:14:40 +0200

    add basic lighting effect to UnlitTexture shader

--- OpenGLPuzzleGame/UnlitTextureFS.glsl
+++ OpenGLPuzzleGame/UnlitTextureFS.glsl
@@ -4,8 +4,9 @@ uniform sampler2D color_map;
 uniform vec2 offset;
 uniform vec2 tilling;
 uniform vec3 tint;
+in float intensity;
 void main()
 {
 	vec2 uv=vec2(offset.x+tilling.x*frag_uv.x,offset.y+tilling.y*(1-frag_uv.y));
-	gl_FragColor=texture2D(color_map, uv)*vec4(tint,1);
+	gl_FragColor=texture2D(color_map, uv)*vec4(tint,1)*intensity;
 }
--- OpenGLPuzzleGame/UnlitTextureVS.glsl
+++ OpenGLPuzzleGame/UnlitTextureVS.glsl
@@ -7,8 +7,13 @@ uniform mat4 normal_mat;
 uniform mat4 view_mat;
 uniform mat4 mvp_mat;
 out vec2 frag_uv;
+out float intensity;
 void main()
 {
 	gl_Position=mvp_mat*vec4(vertex_position,1);
 	frag_uv=vertex_uv;
+	//even its unlit shader, I decided to add basic lighting effect
+	vec3 normal=normalize((inverse(transpose(model_mat))*vec4(vertex_normal.xyz,0)).xyz);
+	vec3 lightDirection=normalize(vec3(3,4,-1));
+	intensity=min(1,0.3+max(0,dot(normal,lightDirection)));
 }
